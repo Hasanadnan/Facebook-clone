eaa55edbba09f01771458388feba60c6651b59fd
Hasanadnan <71681524+Hasanadnan@users.noreply.github.com>
2021-10-22 21:44:35 +0600

    first commit

---
+++ connect/DB.php
@@ -0,0 +1,23 @@
+<?php
+
+class DB
+{
+
+    private static function connect()
+    {
+        $pdo = new PDO('mysql:host=localhost; dbname=facebook; charset=utf8mb4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+    public static function query($query, $params = array())
+    {
+        $statement = self::connect()->prepare($query);
+        $statement->execute($params);
+
+        if (explode(' ', $query)[0] == 'SELECT') {
+            $data = $statement->fetchAll();
+            return $data;
+        }
+    }
+}
---
+++ core/load.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'database/connection.php';
+include 'classes/users.php';
+include 'classes/post.php';
+
+global $pdo;
+
+$loadFromUser = new User($pdo);
+$loadFromPost = new Post($pdo);
+
+define("BASE_URL", "http://localhost:8080/facebook/");
